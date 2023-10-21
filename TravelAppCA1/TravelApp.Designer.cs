eae367b8e6f227e3fc142738e50229dd11aa70f1
luanakimley <d00234604@student.dkit.ie>
2023-10-21 16:19:24 +0100

    :lipstick: Develop structure for flight data form

--- TravelAppCA1/TravelApp.Designer.cs
+++ TravelAppCA1/TravelApp.Designer.cs
@@ -28,32 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.originComboBox = new System.Windows.Forms.ComboBox();
+            this.destinationComboBox = new System.Windows.Forms.ComboBox();
+            this.departureDate = new System.Windows.Forms.DateTimePicker();
+            this.returnDate = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
-            // label1
+            // button1
             // 
-            this.label1.Location = new System.Drawing.Point(82, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(641, 363);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.button1.Location = new System.Drawing.Point(308, 1058);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // Form1
+            // listBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(69, 433);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(950, 388);
+            this.listBox1.TabIndex = 2;
+            // 
+            // originComboBox
+            // 
+            this.originComboBox.DropDownHeight = 100;
+            this.originComboBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.originComboBox.FormattingEnabled = true;
+            this.originComboBox.IntegralHeight = false;
+            this.originComboBox.ItemHeight = 45;
+            this.originComboBox.Location = new System.Drawing.Point(69, 65);
+            this.originComboBox.Margin = new System.Windows.Forms.Padding(5);
+            this.originComboBox.Name = "originComboBox";
+            this.originComboBox.Size = new System.Drawing.Size(264, 53);
+            this.originComboBox.TabIndex = 3;
+            this.originComboBox.Text = "From";
+            // 
+            // destinationComboBox
+            // 
+            this.destinationComboBox.DropDownHeight = 100;
+            this.destinationComboBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.destinationComboBox.FormattingEnabled = true;
+            this.destinationComboBox.IntegralHeight = false;
+            this.destinationComboBox.ItemHeight = 45;
+            this.destinationComboBox.Location = new System.Drawing.Point(364, 65);
+            this.destinationComboBox.Margin = new System.Windows.Forms.Padding(5);
+            this.destinationComboBox.Name = "destinationComboBox";
+            this.destinationComboBox.Size = new System.Drawing.Size(264, 53);
+            this.destinationComboBox.TabIndex = 4;
+            this.destinationComboBox.Text = "To";
+            // 
+            // departureDate
+            // 
+            this.departureDate.CalendarFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.departureDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.departureDate.Location = new System.Drawing.Point(667, 68);
+            this.departureDate.Name = "departureDate";
+            this.departureDate.Size = new System.Drawing.Size(530, 50);
+            this.departureDate.TabIndex = 5;
+            // 
+            // returnDate
+            // 
+            this.returnDate.CalendarFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.returnDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.returnDate.Location = new System.Drawing.Point(1232, 68);
+            this.returnDate.Name = "returnDate";
+            this.returnDate.Size = new System.Drawing.Size(517, 50);
+            this.returnDate.TabIndex = 6;
+            // 
+            // TravelApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(2339, 1249);
+            this.Controls.Add(this.returnDate);
+            this.Controls.Add(this.departureDate);
+            this.Controls.Add(this.destinationComboBox);
+            this.Controls.Add(this.originComboBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "TravelApp";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+        private ComboBox originComboBox;
+        private ComboBox destinationComboBox;
+        private DateTimePicker departureDate;
+        private DateTimePicker returnDate;
     }
 }
