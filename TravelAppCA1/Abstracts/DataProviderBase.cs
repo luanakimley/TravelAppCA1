94feae79536595c76bb9c292e4495b7bc07fe2cb
luanakimley <d00234604@student.dkit.ie>
2023-10-24 03:06:02 +0100

    :recycle: Create abstract classes for fetching APIs

---
+++ TravelAppCA1/Abstracts/DataProviderBase.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+
+namespace TravelAppCA1
+{
+    public abstract class DataProviderBase : IFlightOfferProvider, IAirportDataProvider, ICurrencyDataProvider
+    {
+        protected string baseUrl;
+        protected string apiKey;
+
+        public DataProviderBase(string baseUrlVal, string apiKeyVal)
+        {
+            baseUrl = baseUrlVal;
+            apiKey = apiKeyVal;
+        }
+
+        public virtual FlightData[] GetFlightOffers(bool isMock, string originLocationCode, string destinationLocationCode,
+            DateTime departureDate, int numAdults, int numChildren, TravelClassEnum travelClass, string currencyCode, bool nonStop)
+        { return new FlightData[1]; }
+
+        public virtual Airport[] GetAirportList(bool isMock)
+        { return new Airport[1]; }
+
+        public virtual List<Currency> GetCurrenciesList(bool isMock)
+        { return new List<Currency>();  }
+
+    }
+}
---
+++ TravelAppCA1/Abstracts/OAuthTokenProviderBase.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using TravelAppCA1;
+
+public abstract class OAuthTokenProviderBase : IFlightOfferOAuthTokenProvider
+{
+    protected RestClient restClient;
+    protected RestRequest restRequest;
+    protected string clientId;
+    protected string clientSecret;
+
+    public OAuthTokenProviderBase(string baseUrl, string requestParams, string clientIdVal, string clientSecretVal)
+    {
+        restClient = new RestClient(baseUrl);
+        restRequest = new RestRequest(requestParams, Method.Post);
+        clientId = clientIdVal;
+        clientSecret = clientSecretVal;
+    }
+    public abstract OAuthResults GetOAuthToken();
+}
